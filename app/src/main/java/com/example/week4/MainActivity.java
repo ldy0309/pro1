638edcda0ce47880f161cfbe442196779be676ea
李丹阳 <lidanyang@lidanyangdeMacBook-Pro.local>
2019-09-17 20:47:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/week4/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.week4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView out;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.abc);
+        out =findViewById(R.id.out);
+
+
+        EditText input =findViewById(R.id.inp);
+        String str = input.getText().toString();
+
+        Button btn = findViewById(R.id.btn);
+
+    }
+
+    public void onClick(View v) {
+        Log.i("main","clicked");
+        EditText input =findViewById(R.id.inp);
+        String str = input.getText().toString();
+        out.setText("Hello ," + str);
+
+    }
+}
